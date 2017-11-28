859c9d21903986d8bfa0afb3f9f7443437079e04
wehrling <fwehrling@yahoo.fr>
2017-11-28 21:09:55 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  answer: string;
+  answerDisplay: string;
+  showSpinner: boolean;
+
+  showAnswer() {
+    this.showSpinner = true;
+
+    setTimeout(() => {
+      this.answerDisplay = this.answer;
+      this.showSpinner = false;
+    }, 2000);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
